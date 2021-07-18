f1810a5eaa414c8791bc0a694aa4fbdbd75dec98
benilyxdd <benilyxdd@gmail.com>
2021-07-18 14:39:17 +0800

    add app selection screen basic

---
+++ screens/AppSelectionScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AppSelectionScreen = (props) => {
+	return (
+		<View>
+			<Text>this is app selection screen</Text>
+		</View>
+	);
+};
+
+export default AppSelectionScreen;
