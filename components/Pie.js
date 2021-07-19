becd9b95e45212883220b956937905e9eaa1a272
benilyxdd <benilyxdd@gmail.com>
2021-07-19 18:26:41 +0800

    add pie

--- components/Pie.js
+++ components/Pie.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import { VictoryPie, VictoryTheme } from "victory-native";
 
 const Pie = (props) => {
 	return (
-		<View>
-			
+		<View stlye={styles.pie}>
+			<VictoryPie
+				data={props.data}
+				innerRadius={90}
+				theme={VictoryTheme.material}
+				width={400}
+				radius={150}
+				style={{
+					labels: {
+						fontSize: 15,
+					},
+				}}
+				labelRadius={100}
+			/>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({});
 
 export default Pie;
--- screens/TabScreen/HomeScreen.js
+++ screens/TabScreen/HomeScreen.js
@@ -2,13 +2,21 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import HeaderText from "../../components/HeaderText";
+import Pie from "../../components/Pie";
 
 const HomeScreen = () => {
 	return (
 		<View style={styles.screen}>
 			<HeaderText title="Home" />
 			<View style={styles.middleScreen}>
-				<View style = {styles.pieChartContainer}>
+				<View style={styles.pieChartContainer}>
+					<Pie
+						data={[
+							{ x: "Cats", y: 35 },
+							{ x: "Dogs", y: 40 },
+							{ x: "Birds", y: 55 },
+						]}
+					/>
 				</View>
 				<Text>This is a Home Screen</Text>
 			</View>
@@ -24,11 +32,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	pieChartContainer: {
-		height: '35%',
-		width: '100%',
-		backgroundColor: 'grey',
-		flexDirection: 'row'
-	}
+		height: "55%",
+		width: "100%",
+		backgroundColor: "grey",
+	},
 });
 
 export default HomeScreen;
