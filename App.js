457d01134a177f50c5b8bdaf1db2edc23ba809b1
benilyxdd <benilyxdd@gmail.com>
2021-07-21 14:42:41 +0800

    add redux

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MainScreen from "./screens/MainScreen";
 
 enableScreens();
 
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer);
+
 export default function App() {
-	return <MainScreen />;
+	return (
+		<Provider store={store}>
+			<MainScreen />
+		</Provider>
+	);
 }
