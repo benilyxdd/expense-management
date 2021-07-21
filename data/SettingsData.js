d2092769a1edf6ab3ac39cd10a48bd4d47bfb66e
benilyxdd <benilyxdd@gmail.com>
2021-07-21 16:46:21 +0800

    make settings screen render dynamically

---
+++ data/SettingsData.js
@@ -0,0 +1,72 @@
+export default [
+	{
+		groupName: "Transactions",
+		children: [
+			{
+				name: "Recurring Transactions",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+		],
+	},
+	{
+		groupName: "Settings",
+		children: [
+			{
+				name: "Theme",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+			{
+				name: "hello",
+				onPress: () => {
+					console.log("hi");
+				},
+			},
+		],
+	},
+];
--- screens/TabScreen/SettingsScreen.js
+++ screens/TabScreen/SettingsScreen.js
@@ -3,59 +3,29 @@ import { View, StyleSheet, ScrollView } from "react-native";
 import { List, Divider } from "react-native-paper";
 
 import TouchableListItem from "../../components/TouchableListItem";
+import SettingsData from "../../data/SettingsData";
 
 const SettingScreen = (props) => {
 	return (
 		<View style={styles.screen}>
 			<ScrollView style={styles.test}>
-				<List.Section style={styles.listSection}>
-					<List.Subheader>Transactions</List.Subheader>
-					<Divider />
-					<TouchableListItem
-						title="Recurring Transaction"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-				</List.Section>
-				<List.Section style={styles.listSection}>
-					<List.Subheader>Settings</List.Subheader>
-					<Divider />
-					<TouchableListItem
-						title="Theme"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-					<TouchableListItem
-						title="hello"
-						onPress={() => console.log("hi")}
-					/>
-				</List.Section>
+				{SettingsData.map((group, index) => {
+					return (
+						<List.Section style={styles.listSection} key={index}>
+							<List.Subheader>{group.groupName}</List.Subheader>
+							<Divider />
+							{group.children.map((child, index) => {
+								return (
+									<TouchableListItem
+										title={child.name}
+										onPress={child.onPress}
+										key={index}
+									/>
+								);
+							})}
+						</List.Section>
+					);
+				})}
 			</ScrollView>
 		</View>
 	);
