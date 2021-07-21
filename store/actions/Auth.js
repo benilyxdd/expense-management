cc2e83d028c41ae5e35aba2c2d78668380c998e0
benilyxdd <benilyxdd@gmail.com>
2021-07-22 02:58:33 +0800

    uninstall some package and ready to eject

--- store/actions/Auth.js
+++ store/actions/Auth.js
@@ -3,7 +3,29 @@ export const SIGNUP = "SIGNUP";
 export const SIMPLE_LOGIN = "SIMPLE_LOGIN";
 
 export const login = (email, password) => {
-	return { type: LOGIN, email: email, password: password };
+	return async (dispatch) => {
+		// const response = await fetch(
+		// 	"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]",
+		// 	{
+		// 		method: "POST",
+		// 		header: {
+		// 			"Content-type": "application/json",
+		// 		},
+		// 		body: JSON.stringify({
+		// 			email: email,
+		// 			password: password,
+		// 			returnSecureToken: true,
+		// 		}),
+		// 	}
+		// );
+
+		// if (!response.ok) {
+		// 	throw new Error("no");
+		// }
+
+		// const resData = await response.json();
+		return { type: LOGIN };
+	};
 };
 
 export const signup = (email, password) => {
