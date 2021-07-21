6e8c63e5da26a09c6c9a0c3a68073cad9d21894c
benilyxdd <benilyxdd@gmail.com>
2021-07-21 16:14:48 +0800

    update root reducer

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import { Provider } from "react-redux";
 
 import MainScreen from "./screens/MainScreen";
 import AccountOverall from "./store/reducers/AccountOverall";
+import Home from "./store/reducers/Home";
+import Settings from "./store/reducers/Settings";
 
 enableScreens();
 
 const rootReducer = combineReducers({
 	accountOverallScreen: AccountOverall,
+	HomeScreen: Home,
+	SettingsScreen: Settings,
 });
 const store = createStore(rootReducer);
 
