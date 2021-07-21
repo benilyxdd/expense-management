1d34a728f5649fe42571f67d476e75c40047c045
benilyxdd <benilyxdd@gmail.com>
2021-07-21 21:09:09 +0800

    add redux-thunk

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { enableScreens } from "react-native-screens";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import ReduxThunk from 'redux-thunk'
 
 import MainScreen from "./screens/MainScreen";
 import AccountOverall from "./store/reducers/AccountOverall";
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
 	HomeScreen: Home,
 	SettingsScreen: Settings,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
 	return (
