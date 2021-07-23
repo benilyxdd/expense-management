12db6ececbab6f41f3bc16e963f5c06c2a0de45d
benilyxdd <benilyxdd@gmail.com>
2021-07-23 18:31:28 +0800

    google login half (async and await needed - thunk?)

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { useDispatch } from "react-redux";
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
 
-import { simpleLogin } from "../store/actions/Auth";
+import { simpleLogin, googleLogin } from "../store/actions/Auth";
 
 const AuthScreen = (props) => {
 	const dispatch = useDispatch();
+	const googleLogInHandler = () => {
+		dispatch(googleLogin());
+	};
 	const loginHandler = () => {
 		dispatch(simpleLogin());
 	};
-
+	
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.text}>This is auth screen</Text>
 			<Button title="Hello" onPress={loginHandler} />
+			<GoogleSigninButton onPress={googleLogInHandler} />
 		</View>
 	);
 };
