48450cf939817d7018024953ad702a77cc498470
benilyxdd <benilyxdd@gmail.com>
2021-07-25 13:45:19 +0800

    add transaction reducer

--- App.js
+++ App.js
@@ -7,12 +7,14 @@ import thunk from "redux-thunk";
 import MainScreen from "./screens/MainScreen";
 import Auth from "./store/reducers/Auth";
 import AppData from "./store/reducers/AppData";
+import Transactions from "./store/reducers/Transactions";
 
 enableScreens();
 
 const rootReducer = combineReducers({
 	Auth: Auth,
 	AppData: AppData,
+	Transactions: Transactions,
 });
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
