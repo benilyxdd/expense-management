eca70e567f9cacbdd56dfc99e1787427cbf54fb8
benilyxdd <benilyxdd@gmail.com>
2021-08-08 17:27:38 +0800

    style category screen and add delete button

--- screens/SettingsScreen/Categories.js
+++ screens/SettingsScreen/Categories.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Button } from "react-native-paper";
 import { useSelector } from "react-redux";
 
 const Categories = () => {
@@ -8,13 +9,56 @@ const Categories = () => {
 	);
 
 	return (
-		<View>
+		<ScrollView style={styles.screen}>
 			{categoriesList &&
 				categoriesList.map((item, index) => {
-					return <Text key={index}>{item}</Text>;
+					return (
+						<View key={index} style={styles.itemContainer}>
+							<View style={styles.textContainer}>
+								<Text style={styles.text}>{item}</Text>
+							</View>
+							<View style={styles.buttonContainer}>
+								<Button
+									icon="delete"
+									color="red"
+									onPress={() => console.log(item)}
+								>
+									Delete
+								</Button>
+							</View>
+						</View>
+					);
 				})}
-		</View>
+		</ScrollView>
 	);
 };
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+	},
+	itemContainer: {
+		flexDirection: "row",
+		alignItems: "baseline",
+		marginHorizontal: "5%",
+		margin: "3%",
+		padding: "2%",
+		borderRadius: 15,
+		borderColor: "black",
+		borderWidth: 1,
+	},
+	textContainer: {
+		flex: 5,
+		padding: "2%",
+		height: "100%",
+		width: "100%",
+	},
+	buttonContainer: {
+		flex: 3,
+	},
+	text: {
+		fontSize: 20,
+	},
+});
 
 export default Categories;
