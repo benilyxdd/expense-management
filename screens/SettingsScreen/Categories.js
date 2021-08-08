e13859fbd0a77d4bae916dd5bc61d29fd38ada55
benilyxdd <benilyxdd@gmail.com>
2021-08-08 16:31:43 +0800

    remove add category input field from categories list screen

--- screens/SettingsScreen/Categories.js
+++ screens/SettingsScreen/Categories.js
@@ -1,30 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View, Text } from "react-native";
-import { TextInput } from "react-native-paper";
 import { useSelector, useDispatch } from "react-redux";
-
-import {
-	inputCategoryChange,
-	resetInputCategory,
-} from "../../store/actions/Category";
 
 const Categories = () => {
 	const dispatch = useDispatch();
-	const inputCategory = useSelector((state) => state.Category.inputCategory);
 	const categoriesList = useSelector(
 		(state) => state.Auth.userData.basicInfo.categories
 	);
 
-	useEffect(() => {
-		dispatch(resetInputCategory());
-	}, []);
-
 	return (
 		<View>
-			<TextInput
-				value={inputCategory}
-				onChangeText={(input) => dispatch(inputCategoryChange(input))}
-			/>
 			{categoriesList &&
 				categoriesList.map((item, index) => {
 					return <Text key={index}>{item}</Text>;
