08d8c4142c5a564fd3c2762f1c398887a5125baf
benilyxdd <benilyxdd@gmail.com>
2021-08-12 00:55:36 +0800

    build simple submit feedback screen

--- screens/SettingsScreen/SubmitFeedback.js
+++ screens/SettingsScreen/SubmitFeedback.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { TextArea, TextField } from "react-native-ui-lib";
 
 const SubmitFeedback = () => {
 	return (
-		<View>
-			<Text>This is SubmitFeedback screen</Text>
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextField placeholder="subject" />
+				<TextArea placeholder="add something..." />
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		alignItems: "center",
+	},
+	inputContainer: {
+		width: "90%",
+		height: "50%",
+		margin: "3%",
+		borderWidth: 2,
+		borderColor: "black",
+		borderRadius: 15,
+		padding: "3%",
+	},
+});
 
 export default SubmitFeedback;
